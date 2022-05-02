922bd17565d76e2f6b2e3a8d8931ecd8c7f6c55b
Ories <tellt1@126.com>
2022-05-02 11:38:15 +0800

    init

---
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -0,0 +1,15 @@
+package com.github.hcsp;
+
+import com.github.hcsp.pet.Cat;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HomeTest {
+    @Test
+    public void testNewCat() throws Exception {
+        Method method = Home.class.getDeclaredMethod("newCat");
+        method.setAccessible(true);
+        Assertions.assertTrue(method.invoke(null) instanceof Cat);
+    }
+}
